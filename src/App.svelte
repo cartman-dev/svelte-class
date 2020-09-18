67c4cfc69902203ecb0b10b68247dc65321456ef
Eric Cartman <66183913+cartman-dev@users.noreply.github.com>
2020-09-18 15:48:27 -0400

    Added Modal

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+	import Modal from './Modal.svelte';
+
 	let people = [
 		{ name: 'yoshi', beltColor: 'black', age: 25, id: 1 },
 		{ name: 'mario', beltColor: 'orange', age: 45, id: 2 },
@@ -10,17 +12,9 @@
 		people = people.filter((person) => person.id != id);
 	};
 
-	let num = 25;
 </script>
 
-{#if num > 20}
-	<p>Greater than 20</p>
-{:else if num > 5}
-	<p>Greater than 5</p>
-{:else}
-	<p>Not greater than 5</p>
-{/if}
-
+<Modal />
 <main>
 	{#each people as person (person.id)}
 		<div>
