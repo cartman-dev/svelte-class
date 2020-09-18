32c10c778a0361c7fa409f7da4566261beb50405
Eric Cartman <66183913+cartman-dev@users.noreply.github.com>
2020-09-18 16:41:43 -0400

    End of lesson-14

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,54 @@
 <script>
-	import Modal from './Modal.svelte';
-	let showModal = false;
-	let toggleModal = () => {
-	  showModal = !showModal;
-	};
-	  let people = [
-	  { name: 'yoshi', beltColour: 'black', age: 25, id: 1 },
-	  { name: 'mario', beltColour: 'orange', age: 45, id: 2 },
-	  { name: 'luigi', beltColour: 'brown', age: 35, id: 3 }
-	];
-	const handleClick = (e, id) => {
-	  people = people.filter(person => person.id != id);
-	  console.log(e);
-	};
-  </script>
-  
-  <Modal message='Hello, Ninjas!' {showModal} on:click={toggleModal} />
-  <main>
-	<button on:click|once={toggleModal}>Open Modal</button>
-	{#each people as person (person.id)}
-	  <div>
-		<h4>{person.name}</h4>
-		{#if person.beltColour === 'black'}
-		  <p><strong>MASTER NINJA</strong></p>
-		{/if}
-		<p>{person.age} years old, {person.beltColour} belt.</p>
-		<button on:click={(e) => handleClick(e, person.id)}>delete</button>
-	  </div>
-	{:else}
-	  <p>There are no people to show...</p>
-	{/each}
-  </main>
-  
-  <style>
-	  main {
-		  text-align: center;
-		  padding: 1em;
-		  max-width: 240px;
-		  margin: 0 auto;
-	  }
-	  @media (min-width: 640px) {
-		  main {
-			  max-width: none;
-		  }
-	  }
-  </style>
+  import Modal from "./Modal.svelte";
+  let showModal = false;
+  let toggleModal = () => {
+    showModal = !showModal;
+  };
+  let people = [
+    { name: "yoshi", beltColour: "black", age: 25, id: 1 },
+    { name: "mario", beltColour: "orange", age: 45, id: 2 },
+    { name: "luigi", beltColour: "brown", age: 35, id: 3 },
+  ];
+  const handleClick = (e, id) => {
+    people = people.filter((person) => person.id != id);
+    console.log(e);
+  };
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<Modal {showModal} on:click={toggleModal}>
+  <h3>Add a New Person</h3>
+  <form>
+    <input type="text" placeholder="name">
+    <input type="text" placeholder="belt color">
+	<button>Add Person</button>
+  </form>
+</Modal>
+<main>
+  <button on:click|once={toggleModal}>Open Modal</button>
+  {#each people as person (person.id)}
+    <div>
+      <h4>{person.name}</h4>
+      {#if person.beltColour === 'black'}
+        <p><strong>MASTER NINJA</strong></p>
+      {/if}
+      <p>{person.age} years old, {person.beltColour} belt.</p>
+      <button on:click={(e) => handleClick(e, person.id)}>delete</button>
+    </div>
+  {:else}
+    <p>There are no people to show...</p>
+  {/each}
+</main>
