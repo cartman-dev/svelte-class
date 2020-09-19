a06f678f5be71f4863f01cc5de550e76a8c8f5e0
Eric Cartman <66183913+cartman-dev@users.noreply.github.com>
2020-09-19 14:05:51 -0400

    End of lesson-20

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,21 @@
 <script>
   import Header from "./components/Header.svelte";
   import Footer from "./components/Footer.svelte";
+  import Tabs from "./shared/Tabs.svelte";
+
+  // tabs
+  let items = ["Current Polls", "Add New Poll"];
+  let activeItem = "Current Polls";
+
+  const tabChange = (e) => {
+    console.log(e.detail);
+    activeItem = e.detail;
+  };
 </script>
 
 <style>
   main {
+    width: 100%;
     max-width: 960px;
     margin: 40px auto;
   }
@@ -12,5 +23,11 @@
 
 <Header />
 <main>
+  <Tabs {items} {activeItem} on:tabChange={tabChange} />
+  {#if activeItem === 'Current Polls'}
+    <p>Poll List</p>
+  {:else if activeItem === 'Add New Poll'}
+    <p>New Poll Form</p>
+  {/if}
 </main>
 <Footer />
