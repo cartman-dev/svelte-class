c5dc401d7a9c73333aaf7fdc97da681f73f43dba
Eric Cartman <66183913+cartman-dev@users.noreply.github.com>
2020-09-19 13:32:04 -0400

    End of lesson-19

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,16 @@
 <script>
-
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
 </script>
 
-<main>
-  <h1>Hello you!</h1>
-</main>
-
 <style>
-
+  main {
+    max-width: 960px;
+    margin: 40px auto;
+  }
 </style>
+
+<Header />
+<main>
+</main>
+<Footer />
