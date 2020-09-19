4d576b49590303113b6e578bdf44c77d74127a07
Eric Cartman <66183913+cartman-dev@users.noreply.github.com>
2020-09-19 07:45:51 -0400

    End of lesson-16

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -11,13 +11,18 @@
 
 <form on:submit|preventDefault={handleSubmit}>
     <input type="text" placeholder="name" bind:value={name}>
-    <input type="text" placeholder="belt color" bind:value={beltColor}>
     <input type="number" placeholder="age" bind:value={age}>
     <label>Skills:</label>
     <input type="checkbox" bind:group={skills} value="fighting">fighting<br>
     <input type="checkbox" bind:group={skills} value="sneaking">sneaking<br>
     <input type="checkbox" bind:group={skills} value="running">running<br>
-    	<button>Add Person</button>
+    <label>Belt color:</label>
+    <select bind:value={beltColor}>
+        <option value="black">black</option>
+        <option value="orange">orange</option>
+        <option value="white">white</option>
+    </select>
+    <button>Add Person</button>
 </form>
 
 <style>
