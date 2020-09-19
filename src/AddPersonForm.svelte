f5400567d138a76f415c6f4588d138a104c1574f
Eric Cartman <66183913+cartman-dev@users.noreply.github.com>
2020-09-19 07:37:29 -0400

    Binding control method 1

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -2,9 +2,12 @@
     let name;
     let beltColor;
     let age;
+    let fighting = false;
+    let sneaking = false;
+    let running = false;
 
     const handleSubmit = () => {
-        console.log(name, beltColor, age);
+        console.log(name, beltColor, age, fighting, sneaking, running);
     }
 </script>
 
@@ -12,6 +15,10 @@
     <input type="text" placeholder="name" bind:value={name}>
     <input type="text" placeholder="belt color" bind:value={beltColor}>
     <input type="number" placeholder="age" bind:value={age}>
+    <label>Skills:</label>
+    <input type="checkbox" bind:checked={fighting}>fighting<br>
+    <input type="checkbox" bind:checked={sneaking}>sneaking<br>
+    <input type="checkbox" bind:checked={running}>running<br>
 	<button>Add Person</button>
 </form>
 
